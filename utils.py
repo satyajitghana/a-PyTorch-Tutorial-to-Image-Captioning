dd87e01b9a16c517da8d5ee3dd001e8ff9686c5a
Satyajit Ghana <satyajitghana7@gmail.com>
2020-11-24 00:12:39 +0530

    update create_input_files

--- utils.py
+++ utils.py
@@ -3,14 +3,23 @@ import numpy as np
 import h5py
 import json
 import torch
-from scipy.misc import imread, imresize
+
+# from scipy.misc import imread, imresize
 from tqdm import tqdm
 from collections import Counter
 from random import seed, choice, sample
+from PIL import Image
 
 
-def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_image, min_word_freq, output_folder,
-                       max_len=100):
+def create_input_files(
+    dataset,
+    karpathy_json_path,
+    image_folder,
+    captions_per_image,
+    min_word_freq,
+    output_folder,
+    max_len=100,
+):
     """
     Creates input files for training, validation, and test data.
 
@@ -23,10 +32,10 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
     :param max_len: don't sample captions longer than this length
     """
 
-    assert dataset in {'coco', 'flickr8k', 'flickr30k'}
+    assert dataset in {"coco", "flickr8k", "flickr30k"}
 
     # Read Karpathy JSON
-    with open(karpathy_json_path, 'r') as j:
+    with open(karpathy_json_path, "r") as j:
         data = json.load(j)
 
     # Read image paths and captions for each image
@@ -38,27 +47,30 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
     test_image_captions = []
     word_freq = Counter()
 
-    for img in data['images']:
+    for img in data["images"]:
         captions = []
-        for c in img['sentences']:
+        for c in img["sentences"]:
             # Update word frequency
-            word_freq.update(c['tokens'])
-            if len(c['tokens']) <= max_len:
-                captions.append(c['tokens'])
+            word_freq.update(c["tokens"])
+            if len(c["tokens"]) <= max_len:
+                captions.append(c["tokens"])
 
         if len(captions) == 0:
             continue
 
-        path = os.path.join(image_folder, img['filepath'], img['filename']) if dataset == 'coco' else os.path.join(
-            image_folder, img['filename'])
+        path = (
+            os.path.join(image_folder, img["filepath"], img["filename"])
+            if dataset == "coco"
+            else os.path.join(image_folder, img["filename"])
+        )
 
-        if img['split'] in {'train', 'restval'}:
+        if img["split"] in {"train", "restval"}:
             train_image_paths.append(path)
             train_image_captions.append(captions)
-        elif img['split'] in {'val'}:
+        elif img["split"] in {"val"}:
             val_image_paths.append(path)
             val_image_captions.append(captions)
-        elif img['split'] in {'test'}:
+        elif img["split"] in {"test"}:
             test_image_paths.append(path)
             test_image_captions.append(captions)
 
@@ -70,30 +82,46 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
     # Create word map
     words = [w for w in word_freq.keys() if word_freq[w] > min_word_freq]
     word_map = {k: v + 1 for v, k in enumerate(words)}
-    word_map['<unk>'] = len(word_map) + 1
-    word_map['<start>'] = len(word_map) + 1
-    word_map['<end>'] = len(word_map) + 1
-    word_map['<pad>'] = 0
+    word_map["<unk>"] = len(word_map) + 1
+    word_map["<start>"] = len(word_map) + 1
+    word_map["<end>"] = len(word_map) + 1
+    word_map["<pad>"] = 0
 
     # Create a base/root name for all output files
-    base_filename = dataset + '_' + str(captions_per_image) + '_cap_per_img_' + str(min_word_freq) + '_min_word_freq'
+    base_filename = (
+        dataset
+        + "_"
+        + str(captions_per_image)
+        + "_cap_per_img_"
+        + str(min_word_freq)
+        + "_min_word_freq"
+    )
 
     # Save word map to a JSON
-    with open(os.path.join(output_folder, 'WORDMAP_' + base_filename + '.json'), 'w') as j:
+    with open(
+        os.path.join(output_folder, "WORDMAP_" + base_filename + ".json"), "w"
+    ) as j:
         json.dump(word_map, j)
 
     # Sample captions for each image, save images to HDF5 file, and captions and their lengths to JSON files
     seed(123)
-    for impaths, imcaps, split in [(train_image_paths, train_image_captions, 'TRAIN'),
-                                   (val_image_paths, val_image_captions, 'VAL'),
-                                   (test_image_paths, test_image_captions, 'TEST')]:
+    for impaths, imcaps, split in [
+        (train_image_paths, train_image_captions, "TRAIN"),
+        (val_image_paths, val_image_captions, "VAL"),
+        (test_image_paths, test_image_captions, "TEST"),
+    ]:
 
-        with h5py.File(os.path.join(output_folder, split + '_IMAGES_' + base_filename + '.hdf5'), 'a') as h:
+        with h5py.File(
+            os.path.join(output_folder, split + "_IMAGES_" + base_filename + ".hdf5"),
+            "a",
+        ) as h:
             # Make a note of the number of captions we are sampling per image
-            h.attrs['captions_per_image'] = captions_per_image
+            h.attrs["captions_per_image"] = captions_per_image
 
             # Create dataset inside HDF5 file to store images
-            images = h.create_dataset('images', (len(impaths), 3, 256, 256), dtype='uint8')
+            images = h.create_dataset(
+                "images", (len(impaths), 3, 256, 256), dtype="uint8"
+            )
 
             print("\nReading %s images and captions, storing to file...\n" % split)
 
@@ -104,7 +132,10 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
 
                 # Sample captions
                 if len(imcaps[i]) < captions_per_image:
-                    captions = imcaps[i] + [choice(imcaps[i]) for _ in range(captions_per_image - len(imcaps[i]))]
+                    captions = imcaps[i] + [
+                        choice(imcaps[i])
+                        for _ in range(captions_per_image - len(imcaps[i]))
+                    ]
                 else:
                     captions = sample(imcaps[i], k=captions_per_image)
 
@@ -112,11 +143,14 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
                 assert len(captions) == captions_per_image
 
                 # Read images
-                img = imread(impaths[i])
+                # img = imread(impaths[i])
+                img = Image.open(impaths[i])
+                img = img.resize((256, 256))
+                img = np.array(img)
                 if len(img.shape) == 2:
                     img = img[:, :, np.newaxis]
                     img = np.concatenate([img, img, img], axis=2)
-                img = imresize(img, (256, 256))
+                # img = imresize(img, (256, 256))
                 img = img.transpose(2, 0, 1)
                 assert img.shape == (3, 256, 256)
                 assert np.max(img) <= 255
@@ -126,8 +160,12 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
 
                 for j, c in enumerate(captions):
                     # Encode captions
-                    enc_c = [word_map['<start>']] + [word_map.get(word, word_map['<unk>']) for word in c] + [
-                        word_map['<end>']] + [word_map['<pad>']] * (max_len - len(c))
+                    enc_c = (
+                        [word_map["<start>"]]
+                        + [word_map.get(word, word_map["<unk>"]) for word in c]
+                        + [word_map["<end>"]]
+                        + [word_map["<pad>"]] * (max_len - len(c))
+                    )
 
                     # Find caption lengths
                     c_len = len(c) + 2
@@ -136,13 +174,27 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
                     caplens.append(c_len)
 
             # Sanity check
-            assert images.shape[0] * captions_per_image == len(enc_captions) == len(caplens)
+            assert (
+                images.shape[0] * captions_per_image
+                == len(enc_captions)
+                == len(caplens)
+            )
 
             # Save encoded captions and their lengths to JSON files
-            with open(os.path.join(output_folder, split + '_CAPTIONS_' + base_filename + '.json'), 'w') as j:
+            with open(
+                os.path.join(
+                    output_folder, split + "_CAPTIONS_" + base_filename + ".json"
+                ),
+                "w",
+            ) as j:
                 json.dump(enc_captions, j)
 
-            with open(os.path.join(output_folder, split + '_CAPLENS_' + base_filename + '.json'), 'w') as j:
+            with open(
+                os.path.join(
+                    output_folder, split + "_CAPLENS_" + base_filename + ".json"
+                ),
+                "w",
+            ) as j:
                 json.dump(caplens, j)
 
 
@@ -166,8 +218,8 @@ def load_embeddings(emb_file, word_map):
     """
 
     # Find embedding dimension
-    with open(emb_file, 'r') as f:
-        emb_dim = len(f.readline().split(' ')) - 1
+    with open(emb_file, "r") as f:
+        emb_dim = len(f.readline().split(" ")) - 1
 
     vocab = set(word_map.keys())
 
@@ -177,11 +229,13 @@ def load_embeddings(emb_file, word_map):
 
     # Read embedding file
     print("\nLoading embeddings...")
-    for line in open(emb_file, 'r'):
-        line = line.split(' ')
+    for line in open(emb_file, "r"):
+        line = line.split(" ")
 
         emb_word = line[0]
-        embedding = list(map(lambda t: float(t), filter(lambda n: n and not n.isspace(), line[1:])))
+        embedding = list(
+            map(lambda t: float(t), filter(lambda n: n and not n.isspace(), line[1:]))
+        )
 
         # Ignore word if not in train_vocab
         if emb_word not in vocab:
@@ -200,13 +254,22 @@ def clip_gradient(optimizer, grad_clip):
     :param grad_clip: clip value
     """
     for group in optimizer.param_groups:
-        for param in group['params']:
+        for param in group["params"]:
             if param.grad is not None:
                 param.grad.data.clamp_(-grad_clip, grad_clip)
 
 
-def save_checkpoint(data_name, epoch, epochs_since_improvement, encoder, decoder, encoder_optimizer, decoder_optimizer,
-                    bleu4, is_best):
+def save_checkpoint(
+    data_name,
+    epoch,
+    epochs_since_improvement,
+    encoder,
+    decoder,
+    encoder_optimizer,
+    decoder_optimizer,
+    bleu4,
+    is_best,
+):
     """
     Saves model checkpoint.
 
@@ -220,18 +283,20 @@ def save_checkpoint(data_name, epoch, epochs_since_improvement, encoder, decoder
     :param bleu4: validation BLEU-4 score for this epoch
     :param is_best: is this checkpoint the best so far?
     """
-    state = {'epoch': epoch,
-             'epochs_since_improvement': epochs_since_improvement,
-             'bleu-4': bleu4,
-             'encoder': encoder,
-             'decoder': decoder,
-             'encoder_optimizer': encoder_optimizer,
-             'decoder_optimizer': decoder_optimizer}
-    filename = 'checkpoint_' + data_name + '.pth.tar'
+    state = {
+        "epoch": epoch,
+        "epochs_since_improvement": epochs_since_improvement,
+        "bleu-4": bleu4,
+        "encoder": encoder,
+        "decoder": decoder,
+        "encoder_optimizer": encoder_optimizer,
+        "decoder_optimizer": decoder_optimizer,
+    }
+    filename = "checkpoint_" + data_name + ".pth.tar"
     torch.save(state, filename)
     # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
     if is_best:
-        torch.save(state, 'BEST_' + filename)
+        torch.save(state, "BEST_" + filename)
 
 
 class AverageMeter(object):
@@ -265,8 +330,8 @@ def adjust_learning_rate(optimizer, shrink_factor):
 
     print("\nDECAYING learning rate.")
     for param_group in optimizer.param_groups:
-        param_group['lr'] = param_group['lr'] * shrink_factor
-    print("The new learning rate is %f\n" % (optimizer.param_groups[0]['lr'],))
+        param_group["lr"] = param_group["lr"] * shrink_factor
+    print("The new learning rate is %f\n" % (optimizer.param_groups[0]["lr"],))
 
 
 def accuracy(scores, targets, k):
